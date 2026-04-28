651827ffd6fe61d8a39bfe88b7f2a8bc6af3cfe5
Einebulldogge <chrismcfarland1016@gmail.com>
2026-04-28 19:06:21 -0400

    Getting the colors down now.

--- Scripts/meta_tags.js
+++ Scripts/meta_tags.js
@@ -12,6 +12,15 @@
     var link = document.createElement('link');
     link.type = 'text/css';
     link.rel = 'stylesheet';
-    link.href = "Styles/main_page.css";
+    link.href = "Styles/default.css";
     document.getElementsByTagName('head')[0].appendChild(link);
 })();
+
+//font from google fonts (Manufacturing Consent)
+const fontLink = document.createElement('link');
+fontLink.rel = 'stylesheet';
+fontLink.href = "https://fonts.googleapis.com/css2?family=Manufacturing+Consent&display=swap";
+
+document.head.appendChild(fontLink);
+
+
